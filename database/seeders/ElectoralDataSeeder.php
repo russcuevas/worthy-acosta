678aa2d741cf2cf7148118efbef3efd6f16887c6
Russel <russelcuevas0@gmail.com>
2026-09-24 16:39:55 +0800

    v1-electoral-data-module-updating

--- database/seeders/ElectoralDataSeeder.php
+++ database/seeders/ElectoralDataSeeder.php
@@ -20,133 +20,43 @@ class ElectoralDataSeeder extends Seeder
 
         $candidatesByYear = [
             '2025' => [
-                'Mayor' => [
-                    ['name' => 'Worthy Acosta', 'color' => '#075998'],
-                    ['name' => 'Ace Jello Concepcion', 'color' => '#E53935'],
-                    ['name' => 'Jocelyn Castaneda', 'color' => '#2E7D32'],
-                ],
-                'Vice Mayor' => [
-                    ['name' => 'Angel Peliglorio', 'color' => '#075998'],
-                    ['name' => 'Lito Del Rosario', 'color' => '#E53935'],
-                    ['name' => 'Danilo Rubia', 'color' => '#FF9800'],
-                ],
-                'Governor' => [
-                    ['name' => 'Jose Enrique Garcia III', 'color' => '#2196F3'],
-                    ['name' => 'Rogelio Valenzuela', 'color' => '#8E24AA'],
-                ],
-                'Congressman' => [
-                    ['name' => 'Albert Garcia', 'color' => '#2196F3'],
-                    ['name' => 'Antonio Roman Jr.', 'color' => '#E53935'],
-                ],
-                'Councilors' => [
-                    ['name' => 'Ivan Ricafrente', 'color' => '#075998'],
-                    ['name' => 'Ronald Arcenal', 'color' => '#075998'],
-                    ['name' => 'Manny Valencia', 'color' => '#E53935'],
-                    ['name' => 'Tito Catipon', 'color' => '#E53935'],
-                    ['name' => 'Joey Carandang', 'color' => '#2E7D32'],
-                    ['name' => 'Susan Banzon', 'color' => '#2E7D32'],
-                ]
+                'Mayor' => [],
+                'Vice Mayor' => [],
+                'Governor' => [],
+                'Congressman' => [],
+                'Councilors' => [],
             ],
             '2023' => [
-                'Barangay Captain' => [
-                    ['name' => 'Candidate Alpha', 'color' => '#2196F3'],
-                    ['name' => 'Candidate Beta', 'color' => '#E53935'],
-                    ['name' => 'Candidate Gamma', 'color' => '#2E7D32'],
-                ],
-                'Barangay Kagawads' => [
-                    ['name' => 'Team Pagbabago (Unified)', 'color' => '#075998'],
-                    ['name' => 'Team Serbisyong Tapat', 'color' => '#E53935'],
-                ]
+                'Barangay Captain' => [],
+                'Barangay Kagawads' => [],
             ],
             '2022' => [
-                'Mayor' => [
-                    ['name' => 'Ace Jello Concepcion', 'color' => '#E53935'],
-                    ['name' => 'Jocelyn Castaneda', 'color' => '#2E7D32'],
-                    ['name' => 'Worthy Acosta', 'color' => '#075998'],
-                ],
-                'Vice Mayor' => [
-                    ['name' => 'Lito Del Rosario', 'color' => '#E53935'],
-                    ['name' => 'Angel Peliglorio', 'color' => '#075998'],
-                ],
-                'Governor' => [
-                    ['name' => 'Jose Enrique Garcia III', 'color' => '#2196F3'],
-                    ['name' => 'Herminio Aquino', 'color' => '#FF9800'],
-                ],
-                'Congressman' => [
-                    ['name' => 'Albert Garcia', 'color' => '#2196F3'],
-                    ['name' => 'Maria Victoria Sy', 'color' => '#E53935'],
-                ],
-                'Councilors' => [
-                    ['name' => 'Ivan Ricafrente', 'color' => '#075998'],
-                    ['name' => 'Ronald Arcenal', 'color' => '#E53935'],
-                    ['name' => 'Manny Valencia', 'color' => '#2E7D32'],
-                ]
+                'Mayor' => [],
+                'Vice Mayor' => [],
+                'Governor' => [],
+                'Congressman' => [],
+                'Councilors' => [],
             ],
             '2019' => [
-                'Mayor' => [
-                    ['name' => 'Jocelyn Castaneda', 'color' => '#2E7D32'],
-                    ['name' => 'Ace Jello Concepcion', 'color' => '#E53935'],
-                ],
-                'Vice Mayor' => [
-                    ['name' => 'Lito Del Rosario', 'color' => '#E53935'],
-                    ['name' => 'Danilo Rubia', 'color' => '#FF9800'],
-                ],
-                'Governor' => [
-                    ['name' => 'Albert Garcia', 'color' => '#2196F3'],
-                    ['name' => 'Dante Manalili', 'color' => '#8E24AA'],
-                ],
-                'Congressman' => [
-                    ['name' => 'Jose Enrique Garcia III', 'color' => '#2196F3'],
-                    ['name' => 'Alexis Rivera', 'color' => '#E53935'],
-                ],
-                'Councilors' => [
-                    ['name' => 'Team Castaneda Alliance', 'color' => '#2E7D32'],
-                    ['name' => 'Team Concepcion Coalition', 'color' => '#E53935'],
-                ]
+                'Mayor' => [],
+                'Vice Mayor' => [],
+                'Governor' => [],
+                'Congressman' => [],
+                'Councilors' => [],
             ],
             '2016' => [
-                'Mayor' => [
-                    ['name' => 'Ace Jello Concepcion', 'color' => '#E53935'],
-                    ['name' => 'Jesse Concepcion', 'color' => '#2196F3'],
-                ],
-                'Vice Mayor' => [
-                    ['name' => 'Lito Del Rosario', 'color' => '#E53935'],
-                    ['name' => 'Angel Peliglorio', 'color' => '#075998'],
-                ],
-                'Governor' => [
-                    ['name' => 'Albert Garcia', 'color' => '#2196F3'],
-                    ['name' => 'Manuel Mendoza', 'color' => '#FF9800'],
-                ],
-                'Congressman' => [
-                    ['name' => 'Jose Enrique Garcia III', 'color' => '#2196F3'],
-                    ['name' => 'Antonio Roman Jr.', 'color' => '#E53935'],
-                ],
-                'Councilors' => [
-                    ['name' => 'Kabalikat ng Bayan', 'color' => '#E53935'],
-                    ['name' => 'Asenso Mariveles', 'color' => '#2196F3'],
-                ]
+                'Mayor' => [],
+                'Vice Mayor' => [],
+                'Governor' => [],
+                'Congressman' => [],
+                'Councilors' => [],
             ],
             '2013' => [
-                'Mayor' => [
-                    ['name' => 'Jesse Concepcion', 'color' => '#2196F3'],
-                    ['name' => 'Angel Peliglorio', 'color' => '#075998'],
-                ],
-                'Vice Mayor' => [
-                    ['name' => 'Lito Del Rosario', 'color' => '#E53935'],
-                    ['name' => 'Victor Ocon', 'color' => '#2E7D32'],
-                ],
-                'Governor' => [
-                    ['name' => 'Albert Garcia', 'color' => '#2196F3'],
-                    ['name' => 'Ramon Arnaiz', 'color' => '#E53935'],
-                ],
-                'Congressman' => [
-                    ['name' => 'Herminia Roman', 'color' => '#FF9800'],
-                    ['name' => 'Felicito Payumo', 'color' => '#2196F3'],
-                ],
-                'Councilors' => [
-                    ['name' => 'Lakas Mariveles', 'color' => '#2196F3'],
-                    ['name' => 'Liberal Party Team', 'color' => '#FF9800'],
-                ]
+                'Mayor' => [],
+                'Vice Mayor' => [],
+                'Governor' => [],
+                'Congressman' => [],
+                'Councilors' => [],
             ]
         ];
 
